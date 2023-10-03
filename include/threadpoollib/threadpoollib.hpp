b34f4396b4b072ba3d49222e67cc2e61107794e6
SidorovichPavel <sidorovich.pasha@gmail.com>
2023-10-03 11:07:45 +0300

    code format

--- include/threadpoollib/threadpoollib.hpp
+++ include/threadpoollib/threadpoollib.hpp
@@ -11,76 +11,78 @@
 
 #include "datapool.hpp"
 
-namespace threadpool {
-
-class threadpool final {
-public:
-    threadpool(int _Count);
-    ~threadpool();
-
-    template<class Fn, class Obj, class... Args>
-    auto enqueue(Fn&& fn, Obj&& obj, Args&&... args)
-        -> std::future<decltype((obj->*fn)(std::forward<Args>(args)...))>;
-
-    template<class Fn, class... Args>
-    auto enqueue(Fn&& fn, Args&&... args) -> std::future<decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...))>;
-
-private:
-    std::mutex queue_mutex_;
-    std::queue<std::function<void(void)>> tasks_;
-    std::condition_variable condition_;
-    bool stop_pool;
-
-    std::vector<std::thread> workers_;
-};
-
-template<class Fn, class Obj, class... Args>
-auto threadpool::enqueue(Fn&& fn, Obj&& obj, Args&&... args)
-    -> std::future<decltype((obj->*fn)(std::forward<Args>(args)...))>
+namespace threadpool
 {
-    using ret_t = decltype((obj->*fn)(std::forward<Args>(args)...));
 
-    auto pTask = new std::packaged_task<ret_t()>(
-        std::bind(std::forward<Fn>(fn), std::forward<Obj>(obj), std::forward<Args>(args)...));
+    class threadpool final
+    {
+    public:
+        threadpool(int _Count);
+        ~threadpool();
 
-    std::future<ret_t> res = pTask->get_future();
+        template <class Fn, class Obj, class... Args>
+        auto enqueue(Fn &&fn, Obj &&obj, Args &&...args)
+            -> std::future<decltype((obj->*fn)(std::forward<Args>(args)...))>;
 
-    std::unique_lock<std::mutex> locker(queue_mutex_);
-    if (stop_pool)
-        throw std::runtime_error("ThreadPull, push task failed, How did you do it?");
+        template <class Fn, class... Args>
+        auto enqueue(Fn &&fn, Args &&...args) -> std::future<decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...))>;
 
-    tasks_.emplace([pTask] {
+    private:
+        std::mutex queue_mutex_;
+        std::queue<std::function<void(void)>> tasks_;
+        std::condition_variable condition_;
+        bool stop_pool;
+
+        std::vector<std::thread> workers_;
+    };
+
+    template <class Fn, class Obj, class... Args>
+    auto threadpool::enqueue(Fn &&fn, Obj &&obj, Args &&...args)
+        -> std::future<decltype((obj->*fn)(std::forward<Args>(args)...))>
+    {
+        using ret_t = decltype((obj->*fn)(std::forward<Args>(args)...));
+
+        auto pTask = new std::packaged_task<ret_t()>(
+            std::bind(std::forward<Fn>(fn), std::forward<Obj>(obj), std::forward<Args>(args)...));
+
+        std::future<ret_t> res = pTask->get_future();
+
+        std::unique_lock<std::mutex> locker(queue_mutex_);
+        if (stop_pool)
+            throw std::runtime_error("ThreadPull, push task failed. How did you do it?");
+
+        tasks_.emplace([pTask]
+                       {
         (*pTask)();
-        delete pTask;
-    });
-    locker.unlock();
-    condition_.notify_one();
-    return res;
-}
+        delete pTask; });
+        locker.unlock();
+        condition_.notify_one();
+        return res;
+    }
 
-template<class Fn, class... Args>
-auto threadpool::enqueue(Fn&& fn, Args&&... args)
-    -> std::future<decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...))>
-{
-    using ret_t = decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...));
+    template <class Fn, class... Args>
+    auto threadpool::enqueue(Fn &&fn, Args &&...args)
+        -> std::future<decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...))>
+    {
+        using ret_t = decltype(std::forward<Fn>(fn)(std::forward<Args>(args)...));
 
-    auto pTask = new std::packaged_task<ret_t()>(std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...));
+        auto pTask = new std::packaged_task<ret_t()>(std::bind(std::forward<Fn>(fn), std::forward<Args>(args)...));
 
-    std::future<ret_t> res = pTask->get_future();
+        std::future<ret_t> res = pTask->get_future();
 
-    std::unique_lock<std::mutex> locker(queue_mutex_);
-    if (stop_pool)
-        throw std::runtime_error("ThreadPull, push task failed, How did you do it?");
+        std::unique_lock<std::mutex> locker(queue_mutex_);
+        if (stop_pool)
+            throw std::runtime_error("ThreadPull, push task failed. How did you do it?");
 
-    tasks_.emplace([pTask] {
+        tasks_.emplace([pTask]
+                       {
         (*pTask)();
-        delete pTask;
-    });
+        delete pTask; });
 
-    locker.unlock();
-    condition_.notify_one();
+        locker.unlock();
+        condition_.notify_one();
 
-    return res;
-}
+        return res;
+    }
 
 } // namespace threadpool
