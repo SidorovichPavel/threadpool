1fdf9f357fe51fb1358147da3061608266cb7a3f
SidorovichPavel <sidorovich.pasha@gmail.com>
2024-09-21 15:26:20 +0300

    format

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -5,39 +5,33 @@
 
 namespace threadpool {
 
-    threadpool::threadpool(size_t threads_count)
-        :
-        stop_pool_(false)
-    {
-        for (size_t i = 0; i < threads_count; ++i)
-            workers_.emplace_back(std::thread([this]()
-                {
-                    for (;;) {
-                        std::move_only_function<void()> task;
-                        {
-                            std::unique_lock<std::mutex> locker(queue_mutex_);
-                            condition_.wait(locker, [this] { return !tasks_.empty() || stop_pool_; });
-                            if (stop_pool_ && tasks_.empty())
-                                return;
-
-                            task = std::move(tasks_.front());
-                            tasks_.pop();
-                        }
-                        task();
-                    }
-                }));
-    }
-
-    threadpool::~threadpool()
-    {
+threadpool::threadpool(size_t threads_count) : stop_pool_(false) {
+  for (size_t i = 0; i < threads_count; ++i)
+    workers_.emplace_back(std::thread([this]() {
+      for (;;) {
+        std::move_only_function<void()> task;
         {
-            std::lock_guard<std::mutex> locker(queue_mutex_);
-            stop_pool_ = true;
+          std::unique_lock<std::mutex> locker(queue_mutex_);
+          condition_.wait(locker,
+                          [this] { return !tasks_.empty() || stop_pool_; });
+          if (stop_pool_ && tasks_.empty()) return;
+
+          task = std::move(tasks_.front());
+          tasks_.pop();
         }
+        task();
+      }
+    }));
+}
 
-        condition_.notify_all();
-        for (auto& thread : workers_)
-            thread.join();
-    }
+threadpool::~threadpool() {
+  {
+    std::lock_guard<std::mutex> locker(queue_mutex_);
+    stop_pool_ = true;
+  }
 
-} // namespace threadpool
+  condition_.notify_all();
+  for (auto& thread : workers_) thread.join();
+}
+
+}  // namespace threadpool
